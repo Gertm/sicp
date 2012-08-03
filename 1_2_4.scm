5f1e7b569b276aeda78c44bced0212c0db4a70a2
Gert Meulyzer <cel357@gmail.com>
2012-08-03 13:28:17 +0200

    1.2.4

---
+++ 1_2_4.scm
@@ -0,0 +1,25 @@
+(define (expt b n)
+  (write 'i)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+(define (expti b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (write 'i)
+  (if (= counter 0)
+      product
+      (expt-iter b
+                 (- counter 1)
+                 (* b product))))
+
+(define (square x)
+  (* x x))
+
+(define (fast-expt b n)
+  (write 'i)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
