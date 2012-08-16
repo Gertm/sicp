8b23854e1e30c1027f115c50da85cca4c1a85a25
Gert Meulyzer <cel357@gmail.com>
2012-08-16 10:38:11 +0200

    1.38

--- 1_3_3.rkt
+++ 1_3_3.rkt
@@ -106,3 +106,25 @@
            (lambda (i) 1.0)
            5)
 
+
+
+;; Exercise 1.38.  In 1737, the Swiss mathematician Leonhard Euler published a memoir
+;; De Fractionibus Continuis, which included a continued fraction expansion for e - 2,
+;; where e is the base of the natural logarithms. In this fraction, the Ni are all 1,
+;; and the Di are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, .... Write a program
+;;                             1  2  3  4  5  6  7  8  9  10 11
+;; that uses your cont-frac procedure from exercise 1.37 to approximate e, based on
+;; Euler's expansion.
+
+(define (fractibus k)
+  (cont-frac (lambda (x) 1)
+             (lambda (x)
+               (if (= (remainder x 3) 2) 
+                           (/ (+ x 1) 1.5) 
+                           1))
+             k))
+
+
+;; Skipping 1.39. Had enough of this ;) Continuous Fractions are cool though.
+
+
