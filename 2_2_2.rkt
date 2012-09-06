fe7f15c0a36d7ebb1c42c1a93ca80455b7c95083
Gert Meulyzer <cel357@gmail.com>
2012-09-06 22:45:05 +0200

    started 2.32.. too tired, will continue other time

--- 2_2_2.rkt
+++ 2_2_2.rkt
@@ -225,3 +225,22 @@
 
 (define (square-tree tree) (tree-map square tree))
 (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+
+;; Exercise 2.32.  We can represent a set as a list of distinct elements, and we
+;; can represent the set of all subsets of the set as a list of lists. For
+;; example, if the set is (1 2 3), then the set of all subsets is
+;; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3)). Complete the following definition
+;; of a procedure that generates the set of subsets of a set and give a clear
+;; explanation of why it works:
+
+;; (define (subsets s)
+;;   (if (null? s)
+;;       (list nil)
+;;       (let ((rest (subsets (cdr s))))
+;;         (append rest (map <??> rest)))))
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) ) rest)))))
