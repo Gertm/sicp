45f6d291710f94b0dd70c6208a6ad3e4f759dec2
Gert Meulyzer <cel357@gmail.com>
2012-09-06 22:31:40 +0200

    2.30

--- 2_2_2.rkt
+++ 2_2_2.rkt
@@ -178,3 +178,39 @@
   ;; How much do you need to change your programs to convert to the new representation?
 ;; cadr -> cdr
 ;; list? -> pair?
+
+(define (scale-tree tree factor)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree factor))
+        (else (cons (scale-tree (car tree) factor)
+                    (scale-tree (cdr tree) factor)))))
+
+;; Exercise 2.30.  Define a procedure square-tree analogous to the
+;; square-list procedure of exercise 2.21. That is, square-list should
+;; behave as follows:
+
+;; (square-tree
+;;  (list 1
+;;        (list 2 (list 3 4) 5)
+;;        (list 6 7)))
+;; (1 (4 (9 16) 25) (36 49))
+
+;; Define square-tree both directly (i.e., without using any higher-order
+;; procedures) and also by using map and recursion.
+(define (square x) (* x x))
+
+(define (square-tree-direct tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree-direct (car tree))
+                    (square-tree-direct (cdr tree))))))
+
+(square-tree-direct (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree) (if (pair? sub-tree)
+                              (square-tree-map sub-tree)
+                              (square sub-tree)))
+       tree))
+            
+(square-tree-map (list 1 (list 2 (list 3 4) 5) (list 6 7)))
