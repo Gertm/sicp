79cfaebf3d5c5e368988adeed32eb15492badf87
Gert Meulyzer <cel357@gmail.com>
2012-09-10 16:32:18 +0200

    ex 2.33

--- 2.2.3.rkt
+++ 2.2.3.rkt
@@ -2,4 +2,41 @@
 
 (provide (all-defined-out))
 
+(define (square x) (* x x))
 
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)  
+        ((not (pair? tree))
+         (if (odd? tree) (square tree) 0))
+        (else (+ (sum-odd-squares (car tree))
+                 (sum-odd-squares (cdr tree))))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) '())
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+;; Exercise 2.33.  Fill in the missing expressions to complete the following
+;; definitions of some basic list-manipulation operations as accumulations:
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
