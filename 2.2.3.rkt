70a79b459ca636e332926ea67fa55cb65a51fec2
Gert Meulyzer <cel357@gmail.com>
2012-09-15 13:38:50 +0200

    2.35

--- 2.2.3.rkt
+++ 2.2.3.rkt
@@ -66,3 +66,19 @@
               coefficient-sequence))
 
 
+(define (count-leaves x)
+  (cond ((null? x) 0)  
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+;; Exercise 2.35.  Redefine count-leaves from section 2.2.2 as an accumulation:
+
+;; (define (count-leaves t)
+;;   (accumulate <??> <??> (map <??> <??>)))
+(define t '((3 4) (1 2)))
+
+(define (count-leaves2 t)
+  (accumulate + 0 (map (lambda (x)
+                         (if (pair? x) (count-leaves2 x)
+                             1)) t)))
