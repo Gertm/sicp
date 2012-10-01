b3d54ca99c28e7319cc27225a865f8ba6d8481ac
Gert Meulyzer <cel357@gmail.com>
2012-10-01 16:07:33 +0200

    2.39

--- 2.2.3.rkt
+++ 2.2.3.rkt
@@ -181,3 +181,9 @@
 ;;   (fold-right (lambda (x y) <??>) nil sequence))
 ;; (define (reverse sequence)
 ;;   (fold-left (lambda (x y) <??>) nil sequence))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
+
+(define (reversel sequence)
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
